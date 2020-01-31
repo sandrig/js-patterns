ba825cbdb21fd3c6d910a9e4aed5cc8fd2a8acd3
sandrig <sandrig.dev@gmail.com>
2020-01-31 21:16:15 +0300

    refactor: remove semicolons

--- 1_creational/1_constructor.js
+++ 1_creational/1_constructor.js
@@ -1,12 +1,12 @@
 // ES5
 // function Server(name, ip) {
-//   this.name = name;
-//   this.ip = ip;
+//   this.name = name
+//   this.ip = ip
 // }
 //
 // Server.prototype.getUrl = function () {
 //   return `https://${this.ip}:80`
-// };
+// }
 
 // ES6
 class Server {
@@ -20,5 +20,5 @@ class Server {
   }
 }
 
-const aws = new Server('AWS German', '82.21.21.32');
-console.log(aws.getUrl());
+const aws = new Server('AWS German', '82.21.21.32')
+console.log(aws.getUrl())
